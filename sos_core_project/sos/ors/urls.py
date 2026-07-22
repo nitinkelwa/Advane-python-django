8efab3b3cb09e9a670e88775629581c8094e1085
nitinkelwa <nitinkelwa0@gmail.com>
2026-07-22 12:28:42 +0530

    done

---
+++ sos_core_project/sos/ors/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.welcome),
+    path('welcome/', views.welcome),
+    path('signup/', views.user_signup),
+    path('signin/', views.user_signin),
+    path('logout/', views.logout),
+    path('userlist/', views.user_list),
+    path('delete/<int:id>/', views.delete_user),
+    path('save/', views.user_save),
+    path('save/<int:id>/', views.user_save),
+
+]
---
+++ sos_core_project/sos/ors/utility/data_validator.py
@@ -0,0 +1,14 @@
+class DataValidator:
+    @staticmethod
+    def is_null(val):
+        if (val == ''):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def is_not_null(val):
+        if val == None or val == "":
+            return False
+        else:
+            return True
